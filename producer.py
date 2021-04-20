b4af4c3275d202a986ef98107044e65eef689858
vishmasterNCI <x20177470@student.ncirl.ie>
2021-04-20 19:59:03 +0100

    updated producer

--- producer.py
+++ producer.py
@@ -14,13 +14,15 @@ sleep_time=1
 
 
 
-producer = KafkaProducer(bootstrap_servers=[brokers],value_serializer=lambda x: dumps(x).encode('utf-8'))
+producer = KafkaProducer(bootstrap_servers=[brokers],value_serializer=lambda x: dumps(x).encode('utf-8'),batch_size=10)
 
 
 while(True):
     print("Getting new data...")
     tweets=pd.read_csv("tweets_covid.csv")
-    for tex in tweets.to_dict(orient='records'):
-        producer.send(topic, tex)
-        time.sleep(sleep_time)
+    #for tex in tweets.to_dict(orient='records'):
+    tex=tweets.to_dict(orient='records')
+    producer.send(topic, tex)
+    producer.flush(30)
+    #time.sleep(sleep_time)
     break
